6fb1a8cb1dec2184569cd7bac845fdcacd9ddf10
Jorge Arévalo <jorgeas80@gmail.com>
2015-07-19 19:55:07 +0200

    Now we're talking xpath properly... more or less. Still need to get some data from movies, but close enough

--- peliscraper/peliscraper/spiders/filmaffinity_spider.py
+++ peliscraper/peliscraper/spiders/filmaffinity_spider.py
@@ -10,49 +10,67 @@ class FilmaffinitySpider(scrapy.Spider):
     start_urls = ["http://www.filmaffinity.com/es/rdcat.php?id=new_th_es"]
 
     def parse(self, response):
-        items = []
         for node in response.xpath('//div[@id="main-wrapper-rdcat"]'):
-            date = None
 
-            first_date_array = node.xpath('div[@class="rdate-cat rdate-cat-first"]/text()')
-            if first_date_array:
-                date = first_date_array.extract()[0]
-
-            else:
-                date_array = node.xpath('div[@class="rdate-cat"]/text()')
-                if date_array:
-                    date = date_array.extract()[0]
-
-            # We need a date, anyway
-            if not date:
-                continue
-
-            #Each movie released on that date
             for movie_node in node.xpath('div[@class="padding-list fa-shadow"]').xpath('div[@class="movie-card movie-card-1"]'):
 
-
-                movie_release_date = date
-
-                # Movie id
-                movie_id_array = movie_node.xpath('@data-movie-id').extract()
-                if not movie_id_array or not isinstance(movie_id_array, list) or len(movie_id_array) <= 0:
-                    continue
-
-                movie_id = movie_id_array[0]
-
-                # Follow the link to get the rest of the information
+                # Follow the link to get the information from the movie's page
                 movie_link_array = movie_node.xpath('div[@class="mc-info-container"]').xpath('div[@class="mc-title"]/a/@href').extract()
                 if not movie_link_array or not isinstance(movie_link_array, list) or len(movie_link_array) <= 0:
                     continue
 
                 movie_link = movie_link_array[0]
 
-                # Build item object with collected values
-                item = PeliscraperItem()
+                url = response.urljoin(movie_link)
 
-                item['movie_release_date'] = movie_release_date
-                item['movie_id'] = movie_id
+                yield scrapy.Request(url, callback=self.parse_dir_contents)
 
-                items.append(item)
 
-        print items
+
+    def parse_dir_contents(self, response):
+
+        # Movie id
+        # TODO: Extract id from here. Use regular expresion
+        movie_id_array = response.xpath('//head/meta[@property="og:url"]').extract()
+        if not movie_id_array or not isinstance(movie_id_array, list) or len(movie_id_array) <= 0:
+            yield None
+
+        # Movie release date
+        # TODO: This is hard
+        move_release_date_array = response.xpath('//div[@id="movie-categories"]').xpath('a/@href/following-sibling')
+
+        # Movie title
+        movie_title_array = response.xpath('//h1[@id="main-title"]').xpath('span[@itemprop="name"]/text()').extract()
+        if not movie_title_array or not isinstance(movie_title_array, list) or len(movie_title_array) <= 0:
+            yield None
+        movie_title = movie_title_array[0].strip()
+
+        # Rest of fields
+        dt_nodes = response.xpath('//div[@id="left-column"]/dl[@class="movie-info"]/dt')
+
+        for dt_node in dt_nodes:
+            if dt_node.xpath('text()')[0].extract().strip() == u'Título original':
+                movie_original_title = dt_node.xpath('following-sibling::dd/text()')[0].extract().strip()
+            elif dt_node.xpath('text()')[0].extract().strip() == u'Año':
+                movie_year = dt_node.xpath('following-sibling::dd/text()')[0].extract().strip()
+            elif dt_node.xpath('text()')[0].extract().strip() == u'Duración':
+                movie_runtime = dt_node.xpath('following-sibling::dd/text()')[0].extract().strip()
+            elif dt_node.xpath('text()')[0].extract().strip() == u'País':
+                movie_country = dt_node.xpath('following-sibling::dd/text()')[0].extract().strip()
+            elif dt_node.xpath('text()')[0].extract().strip() == u'Sinopsis':
+                movie_plot = dt_node.xpath('following-sibling::dd/text()')[0].extract().strip()
+
+
+
+        # Now, build the item
+        item = PeliscraperItem()
+
+        item['movie_title'] = unicode(movie_title)
+        item['movie_original_title'] = unicode(movie_original_title)
+        item['movie_year'] = unicode(movie_year)
+        item['movie_runtime'] = unicode(movie_runtime)
+        item['movie_country'] = unicode(movie_country)
+        item['movie_plot'] = unicode(movie_plot)
+
+
+        yield item
