a8596898e04fcf108abcd84781f37fb82c342eed
Jorge Arévalo <jorgeas80@gmail.com>
2015-07-19 14:32:58 +0200

    Getting more information using Scrapy. I commit this, but we're changing the approach

--- peliscraper/peliscraper/spiders/filmaffinity_spider.py
+++ peliscraper/peliscraper/spiders/filmaffinity_spider.py
@@ -10,6 +10,7 @@ class FilmaffinitySpider(scrapy.Spider):
     start_urls = ["http://www.filmaffinity.com/es/rdcat.php?id=new_th_es"]
 
     def parse(self, response):
+        items = []
         for node in response.xpath('//div[@id="main-wrapper-rdcat"]'):
             date = None
 
@@ -26,12 +27,32 @@ class FilmaffinitySpider(scrapy.Spider):
             if not date:
                 continue
 
-            # Build item object
-            item = PeliscraperItem()
-
-            item['movie_release_date'] = date
-
-            # Now, the rest of the fields
+            #Each movie released on that date
+            for movie_node in node.xpath('div[@class="padding-list fa-shadow"]').xpath('div[@class="movie-card movie-card-1"]'):
 
 
-            yield item
+                movie_release_date = date
+
+                # Movie id
+                movie_id_array = movie_node.xpath('@data-movie-id').extract()
+                if not movie_id_array or not isinstance(movie_id_array, list) or len(movie_id_array) <= 0:
+                    continue
+
+                movie_id = movie_id_array[0]
+
+                # Follow the link to get the rest of the information
+                movie_link_array = movie_node.xpath('div[@class="mc-info-container"]').xpath('div[@class="mc-title"]/a/@href').extract()
+                if not movie_link_array or not isinstance(movie_link_array, list) or len(movie_link_array) <= 0:
+                    continue
+
+                movie_link = movie_link_array[0]
+
+                # Build item object with collected values
+                item = PeliscraperItem()
+
+                item['movie_release_date'] = movie_release_date
+                item['movie_id'] = movie_id
+
+                items.append(item)
+
+        print items
